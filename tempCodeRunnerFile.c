368c401c98d7720ed81c1e2ca26829868fe79c10
saaadk7 <hashmiisaad@gmail.com>
2023-08-29 17:46:21 +0530

    Square Patterns Completed

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,21 @@
- printf("%d", i);
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n;
+    puts("Enter Number :  ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
